eddd6a7cd836295adc11a91b4e2e37de516e48ee
everandrade <eversonandrade79@gmail.com>
2022-08-29 14:14:39 -0300

    Atualização dos exercícios 1, 2 e 3

---
+++ exercicio2/script.js
@@ -0,0 +1,14 @@
+pergunta1 = "Hoje é dia 2?"
+const dia = prompt(pergunta1)
+pergunta2 = "Você está tomando café?"
+const cafe = prompt(pergunta2)
+pergunta3 = "Seu nome é Chimbalauê?"
+const nome = prompt(pergunta3)
+
+let resposta1 = dia
+let resposta2 = cafe
+let resposta3 = nome
+
+console.log(pergunta1, "R: ", resposta1)
+console.log(pergunta2, "R: ", resposta2)
+console.log(pergunta3, "R: ", resposta3)
